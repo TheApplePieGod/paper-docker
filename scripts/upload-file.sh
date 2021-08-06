9641e4844c2ed86e252c99a81ecbf70f1a1dee5c
TheApplePieGod <fishplaysgame@gmail.com>
2021-08-06 11:28:21 -0500

    upload file disk space check

--- scripts/upload-file.sh
+++ scripts/upload-file.sh
@@ -1,5 +1,12 @@
 TARGET_PATH=$1
 TEMP_NAME=$2
+FILE_SIZE=$3
+
+FREE=$(df -k --output=avail "${TARGET_PATH}" | tail -n1)
+if [ ${FREE} -lt ${FILE_SIZE} ]; then
+  echo >&2 "Not enough space for the file"
+  exit 1
+fi
 
 xxd -r -p - "${TARGET_PATH}/${TEMP_NAME}"
 
